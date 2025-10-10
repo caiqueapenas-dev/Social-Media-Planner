8f265171ee596a7bdeb66ddd4e47e836f2329391
unknown <apps.carloshenrique@gmail.com>
2025-10-10 14:45:28 -0300

    correção de aspas para subir na vercel v3

--- components/providers/theme-provider.tsx
+++ components/providers/theme-provider.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import {
+  ThemeProvider as NextThemesProvider,
+  useTheme as useNextTheme,
+} from "next-themes";
 import { type ThemeProviderProps } from "next-themes";
 
 /**
  * Wrapper do ThemeProvider do next-themes
- * Suporta props como:
- * - attribute="class"
- * - defaultTheme="system"
- * - enableSystem
- * - disableTransitionOnChange
  */
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
+
+/**
+ * Hook customizado para usar tema
+ */
+export const useTheme = useNextTheme;
